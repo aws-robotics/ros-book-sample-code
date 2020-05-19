8ce567cd782bb1ae41b496138b9b44cfd800e4f7
Prajakta Gokhale <prajaktg@amazon.com>
2020-05-18 22:43:59 -0700

    Add GitHub Actions
    Signed-off-by: Prajakta Gokhale <prajaktg@amazon.com>

--- src/fake_robot/include/fake_robot/fake_laser.hpp
+++ src/fake_robot/include/fake_robot/fake_laser.hpp
@@ -1,8 +1,27 @@
-// This file is licensed under MIT-0 (https://github.com/aws/mit-0)
-// which can be found in the 'LICENSE' file in this repository.
+// Copyright (c) 2020 Amazon.com, Inc. or its affiliates
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 #ifndef FAKE_ROBOT__FAKE_LASER_HPP_
 #define FAKE_ROBOT__FAKE_LASER_HPP_
+
+#include <memory>
 
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
--- src/fake_robot/src/laser_listener.cpp
+++ src/fake_robot/src/laser_listener.cpp
@@ -1,5 +1,25 @@
-// This file is licensed under MIT-0 (https://github.com/aws/mit-0)
-// which can be found in the 'LICENSE' file in this repository.
+// Copyright (c) 2020 Amazon.com, Inc. or its affiliates
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include <memory>
+#include <utility>
 
 #include "fake_robot/laser_listener.hpp"
 
@@ -14,12 +34,14 @@ LaserListenerNode::LaserListenerNode(const rclcpp::NodeOptions & options)
   auto callback =
     [this](const sensor_msgs::msg::LaserScan::SharedPtr msg) -> void
     {
-      RCLCPP_INFO(this->get_logger(), "Received laser scan %d with %d points",
+      RCLCPP_INFO(
+        this->get_logger(), "Received laser scan %d with %d points",
         received_message_count_++, msg->ranges.size());
     };
   rclcpp::SubscriptionOptions subscription_options;
 
-  subscription_ = create_subscription<sensor_msgs::msg::LaserScan>("scan", qos_request, callback, subscription_options);
+  subscription_ = create_subscription<sensor_msgs::msg::LaserScan>(
+    "scan", qos_request, callback, subscription_options);
 }
 
 }  // namespace fake_robot
